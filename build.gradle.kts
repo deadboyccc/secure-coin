516a9a7d13a716a46fa6e286aec2145158c5f30f
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-05-16 15:54:58 +0300

    Remove secure decision maker implementation and add .gitignore file for build artifacts and sensitive information

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("SecuredecisionKt")
+}
